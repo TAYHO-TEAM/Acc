541f04c1fbe91f58889b664a4d38f273fc3b5d57
Pham Cong Trien <lukaspham22@gmail.com>
2021-04-01 08:32:32 +0700

    update

--- AppService/Web/QuanLyDuAn/Areas/ThongTin/Controllers/PhatHanhController.cs
+++ AppService/Web/QuanLyDuAn/Areas/ThongTin/Controllers/PhatHanhController.cs
@@ -1,10 +1,7 @@
-﻿using Newtonsoft.Json;
-using QuanLyDuAn.Utilities;
+﻿using QuanLyDuAn.Utilities;
 using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
