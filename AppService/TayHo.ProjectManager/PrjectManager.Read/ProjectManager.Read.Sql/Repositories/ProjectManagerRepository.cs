751ad38848cceb2ebd7cc982a37cd76d12152bbb
Pham Cong Trien <lukaspham22@gmail.com>
2021-04-16 15:22:54 +0700

    udpate

--- AppService/TayHo.ProjectManager/PrjectManager.Read/ProjectManager.Read.Sql/Repositories/ProjectManagerRepository.cs
+++ AppService/TayHo.ProjectManager/PrjectManager.Read/ProjectManager.Read.Sql/Repositories/ProjectManagerRepository.cs
@@ -355,6 +355,9 @@ namespace ProjectManager.Read.Sql.Repositories
                 case nameof(_dbContext.DocumentReleasedLogDetail):
                     orders = _dbContext.DocumentReleasedLogDetail;
                     break;
+                case nameof(_dbContext.TemplateMail):
+                    orders = _dbContext.TemplateMail;
+                    break;
                 case nameof(_dbContext.NotifyAccountDetail):
                     orders = _dbContext.NotifyAccountDetail;
                     break;
