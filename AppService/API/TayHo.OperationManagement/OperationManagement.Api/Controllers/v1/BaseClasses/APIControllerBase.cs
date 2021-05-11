85ae51e264e3fce2ceadde606dd9f764caf3ca72
Pham Cong Trien <lukaspham22@gmail.com>
2021-05-11 17:43:13 +0700

    commit

---
+++ AppService/API/TayHo.OperationManagement/OperationManagement.Api/Controllers/v1/BaseClasses/APIControllerBase.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManager.CMD.Infrastructure;
+using Services.Common;
+using System;
+
+
+namespace OperationManagement.Api.Controllers.v1.BaseClasses
+{
+    [ApiVersion("1")]
+    [Route(Settings.CommandAPIDefaultRoute)]
+    [ApiExplorerSettings(GroupName = "v1")]
+    [ApiController]
+    public class APIControllerBase : ControllerBase
+    {
+        protected readonly QuanLyDuAnContext _dbContext;
+
+        public APIControllerBase()
+        {
+
+        }
+    }
+}
